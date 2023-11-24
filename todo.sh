c0c60d235ec79021ae493d0e10c5681c51534623
AdAbd <adezzed@gmail.com>
2023-11-24 14:52:35 +0100

    Ajout des #

--- todo.sh
+++ todo.sh
@@ -27,19 +27,21 @@ while true; do
                 read -p "Entrez le numéro de la tâche à supprimer : " numero_tache
                 
             if [ "$numero_tache" -ge 1 ][ "$numero_tache" -le $(wc -l < tasks.txt) ]; then
-                #Cette condition vérifie si numero_tache est un nombre entier supérieur ou égal à 1 et inférieur ou égal au nombre 
-                #de lignes dans le fichier"tasks.txt". La redirection 2>/dev/null supprime les messages d'erreur liés à une éventuelle entrée non numérique.
+                #Cette condition vérifie si numero_tache est un nombre entier supérieur ou égal à 1 et inférieur ou
+                #égal au nombre de lignes dans le fichier"tasks.txt". 
                    
-                    tache_a_supprimer=$(sed -n "${numero_tache}p" tasks.txt)
-                    #Si la condition précédente est vraie, cette ligne utilise la commande sed pour extraire la tâche correspondante à partir de "tasks.txt" et la stocke dans la variable tache_a_supprimer
+                    tache_a_supprimer=$        (sed -n "${numero_tache}p" tasks.txt)
+                    #Si la condition précédente est vraie, cette ligne utilise la commande sed pour extraire 
+                    #la tâche correspondante à partir de "tasks.txt" et la stocke dans la variable tache_a_supprimer.
                     .
                     
                     sed -i "${numero_tache}d" tasks.txt
-                    #Cette ligne utilise la commande sed avec l'option -i pour effectuer une suppression 
-                    #in-place de la ligne correspondante dans le fichier "tasks.txt".
+                    #Cette ligne utilise la commande sed avec l'option -i pour effectuer une suppression de la ligne 
+                    #correspondante dans le fichier "tasks.txt".
+                    
                     
                     echo "Tâche supprimée : $tache_a_supprimer"
-                    #Enfin, cette ligne affiche un message indiquant que la tâche a été supprimée. 
+                    #Cette ligne affiche un message indiquant que la tâche a été supprimée. 
         
 
 
