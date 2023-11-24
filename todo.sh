4eda69614d0a9c4dcdde3ebc6634585cbd267009
AdAbd <adezzed@gmail.com>
2023-11-24 11:04:31 +0100

    Modif et ajout pour tasks.txt

--- todo.sh
+++ todo.sh
@@ -15,4 +15,21 @@ while true; do
         1)
             read -p "Entrez la description de la nouvelle tâche : " nouvelle_tache
             echo "$nouvelle_tache" >> tasks.txt
-            echo "Tâche ajoutée : $nouvelle_tache"
+            echo "Tâche ajoutée : $nouvelle_tache" 
+            ;;
+        2)
+            if [ -s "tasks.txt" ]; then
+                echo -e "\nListe des tâches :"
+                cat -n tasks.txt
+                read -p "Entrez le numéro de la tâche à supprimer : " numero_tache
+                if [ "$numero_tache" -ge 1 ] 2>/dev/null && [ "$numero_tache" -le $(wc -l < tasks.txt) ]; then
+                    tache_a_supprimer=$(sed -n "${numero_tache}p" tasks.txt)
+                    sed -i "${numero_tache}d" tasks.txt
+                    echo "Tâche supprimée : $tache_a_supprimer"
+                else
+                    echo "Numéro de tâche invalide. Veuillez réessayer."
+                fi
+            else
+                echo "Aucune tâche enregistrée."
+            fi
+            ;;
