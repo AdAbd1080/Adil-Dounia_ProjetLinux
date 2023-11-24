dd6103091e1ba2425846e914d09ab919a3f06fd8
AdAbd <adezzed@gmail.com>
2023-11-24 14:35:06 +0100

    Modification

--- todo.sh
+++ todo.sh
@@ -20,13 +20,30 @@ while true; do
         2)
             if [ -s "tasks.txt" ]; then # vérifie si le fichier "tasks.txt" existe 
                 echo -e "\nListe des tâches :"
-                cat -n tasks.txt
+                cat tasks.txt
+                #Cat pour afficher le contenu du fichier "tasks.txt"
+            
+                
                 read -p "Entrez le numéro de la tâche à supprimer : " numero_tache
-                if [ "$numero_tache" -ge 1 ] 2>/dev/null && [ "$numero_tache" -le $(wc -l < tasks.txt) ]; then
+                
+            if [ "$numero_tache" -ge 1 ][ "$numero_tache" -le $(wc -l < tasks.txt) ]; then
+                #Cette condition vérifie si numero_tache est un nombre entier supérieur ou égal à 1 et inférieur ou égal au nombre 
+                #de lignes dans le fichier"tasks.txt". La redirection 2>/dev/null supprime les messages d'erreur liés à une éventuelle entrée non numérique.
+                   
                     tache_a_supprimer=$(sed -n "${numero_tache}p" tasks.txt)
+                    #Si la condition précédente est vraie, cette ligne utilise la commande sed pour extraire la tâche correspondante à partir de "tasks.txt" et la stocke dans la variable tache_a_supprimer
+                    .
+                    
                     sed -i "${numero_tache}d" tasks.txt
+                    #Cette ligne utilise la commande sed avec l'option -i pour effectuer une suppression 
+                    #in-place de la ligne correspondante dans le fichier "tasks.txt".
+                    
                     echo "Tâche supprimée : $tache_a_supprimer"
-                else
+                    #Enfin, cette ligne affiche un message indiquant que la tâche a été supprimée. 
+        
+
+
+            else
                     echo "Numéro de tâche invalide. Veuillez réessayer."
                 fi
             else
